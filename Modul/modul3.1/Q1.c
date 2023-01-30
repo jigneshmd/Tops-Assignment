ca30851f167105a895e24f73f4f4c2dfa9fd3dc8
jigneshmd <jigneshdankhara82@gmail.com>
2023-01-30 09:21:11 +0530

    first change

---
+++ Modul/modul3.1/Q1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+printf("enter your name\t\t: jignesh dankhara");
+printf("\nenter your birth date\t: 25/08/2001");
+printf("\nenter your age\t\t: 22");
+printf("\nenter your address\t: A-92 tulshi darshan soc yogi chowk");
+
+
+
+
+
+    return 0;
+}
